f5de671d3ebb9d2a978bd7a980c9285b6d2b6e69
Joe Shamon <103616381+jsham042@users.noreply.github.com>
2023-09-15 18:50:21 -0600

    "Implement express server and POST endpoint in server.js"
    This commit introduces a new express server setup and a POST endpoint '/api/leadership' in the server.js file. The endpoint takes a company name as input and uses the OpenAI module to search for leadership information about the company. The server listens on port 3000 by default.
    
    The motivation for this change was to provide a backend service for our React application that can handle API requests and return leadership information about a company based on the company name provided. This is a new implementation, as the server.js file was just created and did not have any previous functionality.
    
    This change is part of a larger task to create a React application that allows users to enter a company name and receive leadership information about the company. Other components of the application, such as the 'App', 'InputField', and 'ResultsTable' components, have also been created or modified as part of this task.

--- src/server.js
+++ src/server.js
@@ -1,1 +1,24 @@
-# This is placeholder content for the file. It will eventually satisfy the task: Create a new file 'server.js' in 'src/'. This file will use Express.js to handle API requests.
+const express = require("express");
+const app = express();
+const OpenAI = require("./OpenAI.js");
+
+app.use(express.json());
+
+app.post("/api/leadership", async (req, res) => {
+  const companyName = req.body.companyName;
+  if (!companyName) {
+    return res.status(400).send({ error: "Company name is required" });
+  }
+
+  try {
+    const leadershipInfo = await OpenAI.searchInternetForInformation(
+      `Leadership of ${companyName}`,
+    );
+    res.send({ leadershipInfo });
+  } catch (error) {
+    res.status(500).send({ error: "Error retrieving leadership information" });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
