8d860c939e442444ff82c214f5c994592728c6f5
Joe Shamon <103616381+jsham042@users.noreply.github.com>
2023-09-15 18:51:00 -0600

    "Implement ResultsTable component with responsive design"
    This commit introduces a new functional component 'ResultsTable' in the file 'src/components/ResultsTable/ResultsTable.js'. The component takes 'data' as a prop and maps over it to create a table. The table is styled using the 'ResultsTable.css' file which is imported in the component file. 
    
    The motivation for this change was to create a component that can display the results in a clean, easy-to-read layout. The table is made responsive by wrapping it in a div and applying appropriate CSS in the 'ResultsTable.css' file. This ensures that the table adapts to various screen sizes, providing a better user experience. 
    
    This implementation differs from the previous placeholder content in the file. The placeholder content was static and did not provide any functionality. The new 'ResultsTable' component is dynamic and functional, capable of displaying varying data in a table format.

--- src/components/ResultsTable/ResultsTable.js
+++ src/components/ResultsTable/ResultsTable.js
@@ -1,1 +1,26 @@
-# This is placeholder content for the file. It will eventually satisfy the task: Create a new React component 'ResultsTable' in 'src/components/ResultsTable/ResultsTable.js'. This component will render a responsive table for displaying the results.
+import React from "react";
+import "./ResultsTable.css";
+
+const ResultsTable = ({ data }) => {
+  const columns = data[0] && Object.keys(data[0]);
+  return (
+    <div>
+      <table className="table">
+        <thead>
+          <tr>{data[0] && columns.map((heading) => <th>{heading}</th>)}</tr>
+        </thead>
+        <tbody>
+          {data.map((row) => (
+            <tr>
+              {columns.map((column) => (
+                <td>{row[column]}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ResultsTable;
