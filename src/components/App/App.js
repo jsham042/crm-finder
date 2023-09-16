7f6ee8bc4bade6744940f713925bc4b68e2bef17
Joe Shamon <103616381+jsham042@users.noreply.github.com>
2023-09-15 18:51:47 -0600

    "Update App component with state and data fetching"
    In response to the need for a main component that handles data fetching and state management, the App component was updated. The changes include the addition of state for data and loading, and an async function handleSearch to fetch data from the server when a company name is submitted. 
    
    The InputField component was added to the render method with a prop for the handleSearch function. This allows the user to input a company name and trigger the data fetching process. 
    
    A conditional rendering was also added to display a loading message while fetching data and the ResultsTable component when the data is loaded. This improves user experience by providing feedback during the data fetching process and displaying the results in a clean, responsive table. 
    
    These changes differ from the previous implementation where there was no state management or data fetching in the App component. The addition of these features makes the App component the main component of the application, handling most of the logic and rendering the necessary components based on the state.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,1 +1,31 @@
-# This is placeholder content for the file. It will eventually satisfy the task: Create a new React component 'App' in 'src/components/App/App.js'. This component will serve as the main component of the application.
+import React, { useState } from "react";
+import InputField from "../InputField/InputField";
+import ResultsTable from "../ResultsTable/ResultsTable";
+
+const App = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const handleSearch = async (companyName) => {
+    setLoading(true);
+    const response = await fetch("/api/leadership", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ companyName }),
+    });
+    const data = await response.json();
+    setData(data.leadershipInfo);
+    setLoading(false);
+  };
+
+  return (
+    <div className="App">
+      <InputField onSearch={handleSearch} />
+      {loading ? <p>Loading...</p> : <ResultsTable data={data} />}
+    </div>
+  );
+};
+
+export default App;
