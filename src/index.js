6be41e9d8a9b900fe30ad2ebfdb1f1739fa73f7e
Joe Shamon <103616381+jsham042@users.noreply.github.com>
2023-09-19 16:03:30 -0600

    "Update import statements and add service worker in src/index.js"
    In response to the task instructions, this commit makes two main changes in the src/index.js file. Firstly, the import statement for ReactDOM has been updated to 'react-dom' from 'react-dom/client'. This change ensures that the correct dependency is imported for the application to function properly.
    
    Secondly, an import statement for the service worker has been added. This was necessary to ensure that the service worker is correctly imported and used in the application. Additionally, the service worker unregister statement has been added at the end of the chunk. This is a crucial step in the lifecycle of a service worker, as it allows the service worker to be updated or removed when necessary.
    
    These changes were motivated by the need to ensure that all dependencies are correctly imported and used in the application, and to maintain and update the state of the application correctly. The changes differ from the previous implementation in that they correct the import statement for ReactDOM and add the import and unregister statements for the service worker.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App/App";
+import * as serviceWorker from "./serviceWorker";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -15,3 +16,8 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
